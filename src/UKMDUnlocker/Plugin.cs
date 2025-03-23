0fdf3f3ff8887207d78aae0324cd355a63fdc4d3
whyis2plus2 <kaydenthememelord420.alt1@gmail.com>
2025-03-23 02:43:25 -0600

    big change for a small fix

--- src/UKMDUnlocker/Plugin.cs
+++ src/UKMDUnlocker/Plugin.cs
@@ -2,15 +2,14 @@
 
 using BepInEx;
 using BepInEx.Bootstrap;
-using BepInEx.Logging;
 using HarmonyLib;
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
@@ -22,90 +21,147 @@ public class Plugin : BaseUnityPlugin
     public const string PLUGIN_NAME = "UKMDUnlocker";
     public const string PLUGIN_VERSION = "0.2.1";
 
+    public const string DIF_NAME = "Ultrakill Must Die";
+    public const string DIF_NAME_SHORT = "UKMD";
+
 
     /// <summary> The scene name of the main menu </summary>
-    private const string MAIN_MENU_NAME = "b3e7f2f8052488a45b35549efb98d902";
+    public const string MAIN_MENU_NAME = "b3e7f2f8052488a45b35549efb98d902";
 
-    /// <summary> The scene name of the startup sequence/new save intro </summary>
-    private const string INTRO_NAME = "4f8ecffaa98c2614f89922daf31fa22d";
 
     /// <summary> The "interactable" components of the difficulty select menu (mostly just difficulty buttons and infos) </summary>
-    public static Transform Interactables {private set; get;}
+    public Transform Interactables {private set; get;}
 
-    public static readonly string DifficultyName = GameDifficulty.UKMD.GetDifficultyName();
+    public GameObject UKMDButton = null;
+    public GameObject UKMDInfo = null;
 
-    public static bool HasBananaDifficulty { private set; get; } = false;
+    readonly bool HasBananaDifficulty = Chainloader.PluginInfos.ContainsKey("com.michi.BananaDifficulty");
+
+    /// <summary> The current instance of the plugin, accessable by all parts of the code </summary>
+    public static Plugin Instance;
 
     /// <summary> We need to have an instance of this in order to do patches </summary>
-	public static readonly Harmony Harmony = new(PLUGIN_GUID);
+	readonly Harmony harmony = new(PLUGIN_GUID);
 
-    public static ManualLogSource Log;
+    // TODO: Figure out how to actually get the intro to work
+    /// <summary> The scene name of the startup sequence/new save intro </summary>
+    // private const string INTRO_NAME = "4f8ecffaa98c2614f89922daf31fa22d";
 
     private void Awake()
     {
-        Log = Logger;
-        HasBananaDifficulty = Chainloader.PluginInfos.ContainsKey("com.michi.BananaDifficulty");
-
+        Instance = this;
         SceneManager.activeSceneChanged += OnSceneChange;
 
-        Log.LogInfo("Loading UKMDUnlocker");
-        if (HasBananaDifficulty)
-        {
-            Log.LogInfo("Detected BananasDifficulty");
-            BananaDifficultyManager.Enabled = true;
-        }
-
-        Harmony.PatchAll();
-        Log.LogInfo("Loaded UKMDUnlocker");
+        harmony.PatchAll();
+        Logger.LogInfo($"Loaded {Plugin.PLUGIN_NAME}");
     }
 
-    private void OnSceneChange(Scene last, Scene current)
+    private void OnSceneChange(Scene last, Scene next)
     {
-        LeaderboardProperties.Difficulties[5] = (current.name == MAIN_MENU_NAME)? "UKMD" : DifficultyName;
-        if (current.name is not MAIN_MENU_NAME and not INTRO_NAME) return;
+        LeaderboardProperties.Difficulties[5] = (next.name == MAIN_MENU_NAME)? DIF_NAME_SHORT : DIF_NAME;
+        if (next.name != MAIN_MENU_NAME) return;
 
-        var canvas = (from obj in current.GetRootGameObjects() where obj.name == "Canvas" select obj.transform).First();
+        var canvas = (from obj in next.GetRootGameObjects() where obj.name == "Canvas" select obj).First().transform;
 
         // difficulty buttons and difficulty infos
-        Interactables = (current.name == MAIN_MENU_NAME)
-            ?canvas.Find("Difficulty Select (1)").Find("Interactables")
-            :canvas.Find("Intro").Find("Difficulty Select").Find("Interactables");
+        Interactables = canvas.Find("Difficulty Select (1)/Interactables");
 
         // create the new UKMD button and Info
+        AddInfo();
         AddButton();
 
         // if banana difficulty is avaliable then make any neccessary tweaks to its button and info
-        if (HasBananaDifficulty) BananaDifficultyManager.ModifyButton();
+        // if (HasBananaDifficulty) BananaDifficultyManager.ModifyButton();
     }
 
     /// <summary> Add the UKMD button and info to the difficulty select menu </summary>
     private void AddButton()
     {
-        Log.LogInfo("Adding UKMD Button");
+        KeyValuePair<string, GameObject> FindElem(string name) =>
+            new(name, Interactables.Find(name).gameObject);
 
-        // get the unmodded/real ukmd button
-        var realUKMD = Interactables.Find("V1 Must Die");
+        Logger.LogInfo("Adding UKMD Button...");
+
+        Dictionary<string, GameObject> buttons = new([
+            FindElem("Casual Easy"), // Harmless
+            FindElem("Casual Hard"), // Lenient
+            FindElem("Standard"),
+            FindElem("Violent"),
+            FindElem("Brutal"),
+            FindElem("V1 Must Die"), // Normal UKMD button
+        ]);
+
+        Dictionary<string, GameObject> infos = new([
+            FindElem("Harmless Info"),
+            FindElem("Lenient Info"),
+            FindElem("Standard Info"),
+            FindElem("Violent Info"),
+            FindElem("Brutal Info"),
+        ]);
 
         // clone the brutal button
-        var ukmdButton = Instantiate(Interactables.Find("Brutal").gameObject, Interactables);
-        ukmdButton.GetComponent<DifficultySelectButton>().difficulty = 5;
-        ukmdButton.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = DifficultyName.ToUpper();
-        ukmdButton.transform.position = realUKMD.position;
-        ukmdButton.name = $"{PLUGIN_NAME} UKMD Button";
+        UKMDButton = Instantiate(buttons.GetValueSafe("Brutal"), Interactables);
+        UKMDButton.GetComponent<DifficultySelectButton>().difficulty = 5;
+        UKMDButton.transform.Find("Name").GetComponent<TMP_Text>().text = DIF_NAME.ToUpper();
+        UKMDButton.transform.position = buttons.GetValueSafe("V1 Must Die").transform.position;
+        UKMDButton.name = $"{DIF_NAME_SHORT} Button";
 
-        // set the size of the UKMD button to zero
-        // I can't disable this because the intro scene doesn't allow it
-        realUKMD.localScale = Vector3.zero;
+        // disable the original ukmd button so that it doesn't get in the way
+        buttons.GetValueSafe("V1 Must Die").gameObject.SetActive(false);
 
-        var ukmdInfo = Instantiate(Interactables.Find("Brutal Info").gameObject, Interactables);
-        ukmdInfo.name = $"{PLUGIN_NAME} UKMD Info";
+        // the event triggers that the button uses to show/hide its description
+        var ukmdTrigger = UKMDButton.GetComponent<EventTrigger>();
 
-        var ukmdTitle = ukmdInfo.transform.Find("Title (1)").GetComponent<TextMeshProUGUI>();
-        ukmdTitle.fontSize = 29 /* this is the largest font size that we can use without the text being split into more than one line */;
-        ukmdTitle.text = $"--{DifficultyName.ToUpper()}--";
+        // remove old triggers because those use Brutal's description instead of UKMD's description
+        ukmdTrigger.triggers.Clear();
+
+        // If the info hasn't been created yet, try to create it
+        if (UKMDInfo == null) AddInfo();
+
+        // hide ukmd info if any of the other buttons are hovered over
+        foreach (var button in buttons.Values) {
+            var trigger = button.GetComponent<EventTrigger>();
+            if (!trigger) continue;
+
+            trigger.triggers.Add(
+                CreateTriggerEntry(EventTriggerType.PointerEnter, (data) => UKMDInfo.SetActive(false))
+            );
+        }
+
+        // add new triggers to ukmd button
+        ukmdTrigger.triggers.AddRange([
+            CreateTriggerEntry(EventTriggerType.PointerEnter, (data) =>
+            {
+                UKMDInfo.SetActive(true);
+                foreach (var info in infos.Values) info.SetActive(false);
+            }),
+
+            CreateTriggerEntry(EventTriggerType.PointerExit,  (data) => UKMDInfo.SetActive(false)),
+            CreateTriggerEntry(EventTriggerType.PointerClick, (data) => UKMDInfo.SetActive(false)),
+        ]);
+
+        // add ukmd button to the button activation sequence
+        var activationSequence = Interactables.GetComponent<ObjectActivateInSequence>();
+        activationSequence.objectsToActivate[14 /* index of vanilla ukmd button */] = UKMDButton;
+
+        Logger.LogInfo("Added UKMD Button!");
+    }
+
+    private void AddInfo()
+    {
+        Logger.LogInfo("Adding UKMD Info...");
+
+        UKMDInfo = Instantiate(Interactables.Find("Brutal Info").gameObject, Interactables);
+        UKMDInfo.name = $"{DIF_NAME_SHORT} Info";
+
+        var ukmdTitle = UKMDInfo.transform.Find("Title (1)").GetComponent<TMP_Text>();
+
+        // set the font size to 29 because if it's the default it'll span multiple lines
+        ukmdTitle.fontSize = 29;
+        ukmdTitle.text = $"--{DIF_NAME.ToUpper()}--";
 
         // set the description of UKMD
-        ukmdInfo.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = 
+        UKMDInfo.transform.Find("Text").GetComponent<TMP_Text>().text = 
             """
             <color=yellow>The unfinished version of UKMD in the game's files.</color>
 
@@ -116,52 +172,23 @@ public class Plugin : BaseUnityPlugin
             <b>Recommended for players who have achieved near mastery over the game and are looking for a fitting challenge.</b>
             """;
 
-        // give the user notice about stuff
-        if (HasBananaDifficulty)
-            ukmdInfo.transform.Find("Text").GetComponent<TextMeshProUGUI>().text +=
-                "\n\n<color=yellow>Due to technical reasons, this uses the same save slot as Bananas Difficulty</color>";
-
-
-        // the event triggers that the button uses to show/hide its description
-        var ukmdTrigger = ukmdButton.GetComponent<EventTrigger>();
-
-        // remove old triggers because those use Brutal's description instead of UKMD's description
-        ukmdTrigger.triggers.Clear();
-        
-        // show ukmd info on mouse hover
-        EventTrigger.Entry pointerEnter = new() { eventID = EventTriggerType.PointerEnter };
-        pointerEnter.callback.AddListener((data) => ukmdInfo.SetActive(true));
-
-        // hide ukmd info on mouse exit
-        EventTrigger.Entry pointerExit = new() { eventID = EventTriggerType.PointerExit };
-        pointerExit.callback.AddListener((data) => ukmdInfo.SetActive(false));
-
-        // hide ukmd info when clicked
-        EventTrigger.Entry onClick = new() { eventID = EventTriggerType.PointerClick };
-        onClick.callback.AddListener((data) =>
-        {
-            if (HasBananaDifficulty) BananaDifficultyManager.Enabled = false;
-            ukmdInfo.SetActive(false);
-        });
-
-        // add new triggers to ukmd button
-        ukmdTrigger.triggers.Add(pointerEnter);
-        ukmdTrigger.triggers.Add(pointerExit);
-        ukmdTrigger.triggers.Add(onClick);
-
-        // add ukmd button to the button activation sequence
-        var activationSequence = Interactables.GetComponent<ObjectActivateInSequence>();
-        activationSequence.objectsToActivate[14 /* index of vanilla ukmd button */] = ukmdButton;
+        Logger.LogInfo("Added UKMD Info!");
     }
-}
 
-[HarmonyPatch(typeof(PrefsManager), MethodType.Constructor)]
-static class PrefsManager_Ctor_Patch
-{
-    // documentation comment because I hate when harmony patches go unexplained
-    /// <summary> Postfix that applies to the constructor of PrefsManager, esuring that no instance of said class does a bounds check for difficulty </summary>
-    static void Postfix(ref Dictionary<string, Func<object, object>> ___propertyValidators)
+    private EventTrigger.Entry CreateTriggerEntry(EventTriggerType id, UnityAction<BaseEventData> call)
     {
-        ___propertyValidators.Remove("difficulty");
+        EventTrigger.Entry ret = new() { eventID = id };
+        ret.callback.AddListener(call);
+        return ret;
     }
-};
+
+    [HarmonyPatch(typeof(PrefsManager), "EnsureValid")]
+    static class PrefsManager_EnsureValid_Patch
+    {
+        static void Postfix(ref object __result, string key, object value)
+        {
+            if (key != "difficulty" || value is not int) return;
+            if ((int)value == 5) __result = value;
+        }
+    };
+}
